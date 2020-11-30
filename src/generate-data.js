51c6a949c25cca0ea32fb8c8041d6c9b0ddff17d
Anton Rusak <4231915+arusak@users.noreply.github.com>
2020-11-30 12:03:37 +0300

    Tuning mock data

--- src/generate-data.js
+++ src/generate-data.js
@@ -1,11 +1,14 @@
-const tableData = Array(30).fill(0).map((v, idx) => ({
-    id: idx,
-    text: 'Value #' + idx,
+const NUMBER_OF_WIDGETS = 10;
+
+const tableData = Array(100).fill(0).map((v, idx) => ({
+    id: (idx + 1),
+    text: 'Value #' + (idx + 1),
     value: Math.random() * 1000000
 }));
 
 let counters = [0, 0, 0];
-const widgets = Array(100).fill(0).map((v, idx) => {
+
+const widgets = Array(NUMBER_OF_WIDGETS).fill(0).map(() => {
         let type = 'long_table';
         let rnd = Math.random() * 3;
         if (rnd < 1) {
@@ -15,8 +18,8 @@ const widgets = Array(100).fill(0).map((v, idx) => {
         }
         switch (type) {
             case 'long_table': {
-                let start = Math.random() * 10;
-                let end = start + Math.random() * 90;
+                let start = 0;
+                let end = 1 + Math.random() * 99;
                 return {
                     type,
                     title: 'Long Table Widget #' + ++counters[0],
@@ -24,8 +27,8 @@ const widgets = Array(100).fill(0).map((v, idx) => {
                 };
             }
             case 'short_table': {
-                let start = Math.random() * 50;
-                let end = start + Math.random() * 7 + 3;
+                let start = 0;
+                let end = start + 1 + Math.random() * 9;
                 return {
                     type,
                     title: 'Short Table Widget #' + ++counters[1],
